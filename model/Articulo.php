08f04aaa7cc29e937ec15d68899fdd5f409860a5
chat <chatuseonly@proton.me>
2025-10-30 11:52:11 +0100

    Creacion de los atributos nombre, precio, disponibilidad, categoria y el __constructor()

--- model/Articulo.php
+++ model/Articulo.php
@@ -1,6 +1,17 @@
 <?php
 // Clase Artículo
 class Articulo{
+ 
+    protected $nombre;
+    protected $precio;
+    protected $disponibilidad;
+    protected $categoria;
 
-  
-}
+    public function __construct($nombre, $precio, $disponibilidad, $categoria){
+        $this -> nombre = $nombre;
+        $this -> precio = $precio;
+        $this -> disponibilidad = $disponibilidad;
+        $this -> categoria = $categoria;
+    }
+
+    
