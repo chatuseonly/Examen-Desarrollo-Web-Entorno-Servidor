df7986880bd4be8c59bcfef97489efb870ae8155
chat <chatuseonly@proton.me>
2025-10-30 12:06:23 +0100

    Creación de la clase Bebida, el atribudo $tamano , el __construct, los Getters, los setters y el método mágico __toString() en Bebida.php

--- model/Bebida.php
+++ model/Bebida.php
@@ -1,2 +1,35 @@
 <?php
 // Clase Bebida que hereda de Artículo
+class Bebida extends Articulo{
+     private $tamano;
+     
+     public function __construct($tamano, $nombre, $precio, $disponibilidad, $categoria){
+        parent::__construct($nombre, $precio, $disponibilidad, $categoria);
+        $this -> tamano = $tamano;
+     }
+
+     //GETTERS
+     public function getTamano(){
+        return $this -> tamano;
+     }
+
+     //SETTERS
+
+     public function setTamano($tamano){
+        $this -> tamano = $tamano;
+        return $this;
+     }
+
+     //Método mágico __toString()
+
+     public function __toString(){
+        return "Tamaño" . $this -> tamano . "<br>";
+     }
+
+
+
+
+
+
+
+}
